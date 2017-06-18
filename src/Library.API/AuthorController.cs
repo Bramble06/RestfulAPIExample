6ccec65a40ac47bb44474546d7aaa830500dd5d1
Devon <>
2017-06-18 15:21:34 -0400

    add routing

--- src/Library.API/AuthorController.cs
+++ src/Library.API/AuthorController.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Library.API
 {
-    public class AuthorController
+    [Route("api/authors")]
+    public class AuthorController : Controller
     {
+
+        private ILibraryRepository _ILibraryRepository;
+
+        public AuthorController(ILibraryRepository libraryRepo)
+        {
+            _ILibraryRepository = libraryRepo;
+        }
+
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _ILibraryRepository.GetAuthors();
+
+            return new JsonResult(authorsFromRepo);
+        }
+
     }
 }
